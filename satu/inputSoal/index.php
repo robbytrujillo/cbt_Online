1f05e73464155ea3d816e589ce9006c241966f4c
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-01 07:08:44 +0700

    Latihan CBT Online

--- satu/inputSoal/index.php
+++ satu/inputSoal/index.php
@@ -1,6 +1,16 @@
 <?php
 echo "<br><br><br>
-<form method=\"POST\" enctype=\"multipart/form-data" action=\"simpan.php\">
+<form method=\"POST\" enctype=\"multipart/form-data\" action=\"simpan.php\">
+<table>
+<tr><td>Soal / Pertanyaan </td><td> : <input type=\"text\" style=\"width: 400px;\" name=\"pertanyaan\"></td></tr>
 
+<tr><td>Jawaban A </td><td> : <input type=\"text\" style=\"width: 400px;\" name=\"jawaban_a\"></td></tr>
+<tr><td>Jawaban B </td><td> : <input type=\"text\" style=\"width: 400px;\" name=\"jawaban_b\"></td></tr>
+<tr><td>Jawaban C </td><td> : <input type=\"text\" style=\"width: 400px;\" name=\"jawaban_c\"></td></tr>
+<tr><td>Jawaban D </td><td> : <input type=\"text\" style=\"width: 400px;\" name=\"jawaban_d\"></td></tr>
+
+<tr><td>Jawaban Benar </td><td> : <input type=\"text\" name=\"jawaban_benar\"></td></tr>
+<tr><td colspan=\"2\"><input type=\"submit\" value=\"Simpan\">
+</table>
 </form>";
 ?>
