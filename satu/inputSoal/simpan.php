d58b714cceee022df5f618448a2d673e9eaa7365
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-01 07:23:47 +0700

    Latihan CBT Online

---
+++ satu/inputSoal/simpan.php
@@ -0,0 +1,11 @@
+<?php
+//koneksi ke Database
+include("koneksi.php");
+
+//input soal ke database mini_test
+$input = "INSERT INTO tabel_soal (pertanyaan, pilihan_a, pilhan_b, 
+                                pilihan_c, pilihan_d, jawaban_benar) 
+          VALUES ('$_POST[pertanyaan]', '$_POST[jawaban_a]', '$_POST[jawaban_b]', 
+                            '$_POST[jawaban_c]', '$_POST[jawaban_d]', '$_POST[jawaban_benar]')";
+
+?>
