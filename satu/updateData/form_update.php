48d0482ea5ed78a9a61a14201d219507948e8140
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-10 06:28:03 +0700

    CBT Online v1

--- satu/updateData/form_update.php
+++ satu/updateData/form_update.php
@@ -1,3 +1,13 @@
 <?php
-    include ("koneksi.php");
+    include ("koneksi.php"); //koneksi ke database
+    //Identifikasi data mana yang Akan di ubah
+    $query = "SELECT * FROM tabel_soal WHERE id_soal='$_GET[id]'";
+    $hasil = mysqli_query($konek, $query);
+    $d     = mysqli_fetch_array($hasil);
+    echo "<form method=\"POST\" enctype=\"multipart/form-data\" 
+         action=\"simpan.php\">
+         <table>
+         <tr><td>Soal / Pertanyaan </td></tr> :
+         
+         </table>
 ?>
