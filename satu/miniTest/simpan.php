77628230e2a3ff53a8b5dafae1d7f5ecac80a466
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-22 08:40:03 +0700

    Aplikasi CBT sederhana - Modul Aksi Simpan Data

--- satu/miniTest/simpan.php
+++ satu/miniTest/simpan.php
@@ -8,5 +8,21 @@ $halaman = $_POST['halaman'] + 1;
 if ($module == 'jawaban' and $act == 'input') {
     $input = "INSERT INTO jawaban (id_soal, jawaban) VALUES ('$_POST[id_soal]', '$_POST[id_soal]', '$_POST[jawaban]')";
     mysqli_query($konek, $input);
+
+    header('location: media.php?halaman=' . $halaman);
+}
+//update jawaban
+elseif ($module == 'jawaban' and $act == 'update') {
+    $upadate = "UPDATE jawaban SET jawaban = '$_POST[jawaban]' WHERE id_soal = '$_POST[id_soal]'";
+    mysqli_query($konek, $upadate);
+
+    header('location: media.php?halaman=' . $halaman);
+}
+
+//Selesai test
+elseif ($act == 'selesai') {
+    $query = "DELETE FROM jawaban";
+    mysqli_query($konek, $query);
+    header('location: media.php?halaman=' . $halaman);
 }
 ?>
