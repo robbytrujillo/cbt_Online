536e3f39657079c63ef48c10d0f1dc8c49a1747e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-24 05:56:32 +0700

    Aplikasi CBT sederhana - Modul Aksi Koneksi DB

---
+++ satu/miniTest/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "mini_test";
+
+// Koneksi dan memilih database di server
+$konek = mysqli_connect($server, $username, $password, $database);
+?>
